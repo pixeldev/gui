c6a954689626a6b4307de4cff256df53c777885d
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-07 22:38:15 -0500

    Registered Listeners!

--- Example/src/main/java/team/unnamed/gui/ExamplePlugin.java
+++ Example/src/main/java/team/unnamed/gui/ExamplePlugin.java
@@ -5,10 +5,12 @@ import me.fixeddev.ebcm.parametric.ParametricCommandBuilder;
 import me.fixeddev.ebcm.parametric.ReflectionParametricCommandBuilder;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import team.unnamed.gui.listeners.MenuListeners;
 
 public class ExamplePlugin extends JavaPlugin {
 
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(new MenuListeners(), this);
         registerCommands();
     }
 
