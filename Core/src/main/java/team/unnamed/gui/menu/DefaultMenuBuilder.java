d18a57693143e43cb871a5e03b1904772eb99d38
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-30 20:41:45 -0500

    Simplify and cleanup code, new systems!

---
+++ Core/src/main/java/team/unnamed/gui/menu/DefaultMenuBuilder.java
@@ -0,0 +1,132 @@
+package team.unnamed.gui.menu;
+
+import team.unnamed.gui.menu.action.CloseMenuAction;
+import team.unnamed.gui.menu.action.OpenMenuAction;
+import team.unnamed.gui.item.ItemClickable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class DefaultMenuBuilder implements MenuBuilder {
+
+    private final String title;
+    private int rows;
+
+    private Map<Integer, ItemClickable> items = new HashMap<>();
+
+    private ItemClickable itemClickable;
+    private OpenMenuAction openMenuAction;
+    private CloseMenuAction closeMenuAction;
+
+    private int from = -1;
+    private int to = -1;
+
+    private boolean cancelClick = true;
+
+    protected DefaultMenuBuilder(String title) {
+        this(title, 6);
+    }
+
+    protected DefaultMenuBuilder(String title, int rows) {
+        this.title = title;
+        this.rows = rows;
+    }
+
+    @Override
+    public MenuBuilder setRows(int rows) {
+        this.rows = rows;
+
+        return this;
+    }
+
+    @Override
+    public MenuBuilder fillItem(ItemClickable itemClickable, int from, int to) {
+        this.itemClickable = itemClickable;
+        this.from = from;
+        this.to = to;
+
+        return this;
+    }
+
+    @Override
+    public MenuBuilder setItems(Map<Integer, ItemClickable> items) {
+        this.items = items;
+
+        return this;
+    }
+
+    @Override
+    public MenuBuilder addItem(int slot, ItemClickable itemClickable) {
+        items.put(slot, itemClickable);
+
+        return this;
+    }
+
+    @Override
+    public MenuBuilder openEvent(OpenMenuAction openMenuAction) {
+        this.openMenuAction = openMenuAction;
+
+        return this;
+    }
+
+    @Override
+    public MenuBuilder closeEvent(CloseMenuAction closeMenuAction) {
+        this.closeMenuAction = closeMenuAction;
+
+        return this;
+    }
+
+    @Override
+    public MenuBuilder cancelClick(boolean cancelClick) {
+        this.cancelClick = cancelClick;
+
+        return this;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public int getRows() {
+        return rows;
+    }
+
+    @Override
+    public Map<Integer, ItemClickable> getItems() {
+        return items;
+    }
+
+    @Override
+    public Optional<OpenMenuAction> getOpenMenuAction() {
+        return Optional.ofNullable(openMenuAction);
+    }
+
+    @Override
+    public Optional<CloseMenuAction> getCloseMenuAction() {
+        return Optional.ofNullable(closeMenuAction);
+    }
+
+    @Override
+    public Optional<ItemClickable> getItemToFill() {
+        return Optional.ofNullable(itemClickable);
+    }
+
+    @Override
+    public int getFrom() {
+        return from;
+    }
+
+    @Override
+    public int getTo() {
+        return to;
+    }
+
+    @Override
+    public boolean isCancelClick() {
+        return cancelClick;
+    }
+
+}
