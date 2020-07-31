4853f81bca6ad9e234fd89874e80268466125ea1
ImGalaxy977 <amiranda8695@gmail.com>
2020-07-31 00:46:08 -0500

    Just cleanup code!

--- Core/src/main/java/team/unnamed/gui/item/DefaultItemClickable.java
+++ Core/src/main/java/team/unnamed/gui/item/DefaultItemClickable.java
@@ -1,32 +1,34 @@
 package team.unnamed.gui.item;
 
 import org.bukkit.inventory.ItemStack;
+
 import team.unnamed.gui.button.Button;
 
 public class DefaultItemClickable implements ItemClickable {
 
-  private final int slot;
-  private final ItemStack item;
-  private final Button button;
+    private final int slot;
+    private final ItemStack item;
+    private final Button button;
 
-  public DefaultItemClickable(int slot, ItemStack item, Button button) {
-    this.slot = slot;
-    this.item = item;
-    this.button = button;
-  }
+    public DefaultItemClickable(int slot, ItemStack item, Button button) {
+        this.slot = slot;
+        this.item = item;
+        this.button = button;
+    }
 
-  @Override
-  public int getSlot() {
-    return slot;
-  }
+    @Override
+    public int getSlot() {
+        return slot;
+    }
 
-  @Override
-  public ItemStack getItem() {
-    return item;
-  }
+    @Override
+    public ItemStack getItem() {
+        return item;
+    }
 
-  @Override
-  public Button getButton() {
-    return button;
-  }
+    @Override
+    public Button getButton() {
+        return button;
+    }
+
 }
