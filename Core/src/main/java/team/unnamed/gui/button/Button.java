5159f5481f01983f51fe3861238e039f5123ec1d
FixedDev <gilbertodamian14@gmail.com>
2020-07-31 01:45:01 -0500

    A bit of documentation for the Button

--- Core/src/main/java/team/unnamed/gui/button/Button.java
+++ Core/src/main/java/team/unnamed/gui/button/Button.java
@@ -2,9 +2,18 @@ package team.unnamed.gui.button;
 
 import org.bukkit.event.inventory.InventoryClickEvent;
 
+/**
+ * Represents the action executed when an {@link team.unnamed.gui.item.ItemClickable} is clicked
+ */
 @FunctionalInterface
 public interface Button {
 
+    /**
+     * Executes the code when an {@link team.unnamed.gui.item.ItemClickable} is clicked
+     *
+     * @param event The {@link InventoryClickEvent} for this click
+     * @return If the event should be cancelled or not
+     */
     boolean executeClick(InventoryClickEvent event);
 
 }
