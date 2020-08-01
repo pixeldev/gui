648e631ff5830c16d8efac432db46b37da7fce29
ImGalaxy977 <amiranda8695@gmail.com>
2020-08-01 10:04:04 -0500

    Added documentation to open action and close action!

--- Core/src/main/java/team/unnamed/gui/menu/action/CloseMenuAction.java
+++ Core/src/main/java/team/unnamed/gui/menu/action/CloseMenuAction.java
@@ -5,6 +5,11 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 @FunctionalInterface
 public interface CloseMenuAction {
 
+    /**
+     * A simple action that will be executed while closing the menu
+     *
+     * @param event Event executed while closing the menu
+     */
     void executeClose(InventoryCloseEvent event);
 
 }
