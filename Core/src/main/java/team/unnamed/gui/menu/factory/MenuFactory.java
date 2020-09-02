22a67d233a60969302f776a9ab023b41505810c8
ImGalaxy977 <amiranda8695@gmail.com>
2020-09-02 13:21:01 -0500

    Add cache to constructor, to optimize code.

--- Core/src/main/java/team/unnamed/gui/menu/factory/MenuFactory.java
+++ Core/src/main/java/team/unnamed/gui/menu/factory/MenuFactory.java
@@ -7,6 +7,7 @@ import org.bukkit.inventory.InventoryHolder;
 import team.unnamed.gui.api.menu.MenuData;
 import team.unnamed.gui.api.menu.MenuInventory;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class MenuFactory {
@@ -14,25 +15,25 @@ public class MenuFactory {
     private static final String SERVER_VERSION = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3].substring(1);
 
     private static final Class<?> MENU_CLASS;
+    private static final Constructor<?> MENU_CONSTRUCTOR;
 
     static {
         try {
             MENU_CLASS = Class.forName("team.unnamed.gui.v" + SERVER_VERSION + ".CraftMenuInventory" + SERVER_VERSION);
-        } catch (ClassNotFoundException e) {
+
+            MENU_CONSTRUCTOR = MENU_CLASS.getConstructor(InventoryHolder.class, int.class, String.class, MenuData.class);
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
             throw new IllegalArgumentException("You server version isn't supported to UnnamedGUI!");
         }
     }
 
     public static Inventory get(int size, String title, MenuData data) {
         try {
-            MenuInventory menuInventory = (MenuInventory) MENU_CLASS
-                    .getConstructor(InventoryHolder.class, int.class, String.class, MenuData.class)
+            MenuInventory menuInventory = (MenuInventory) MENU_CONSTRUCTOR
                     .newInstance(null, size, title, data);
 
-            System.out.println("Instance of " + (menuInventory.getMenuInventory() instanceof  MenuInventory));
-
             return menuInventory.getMenuInventory();
-        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
     }
