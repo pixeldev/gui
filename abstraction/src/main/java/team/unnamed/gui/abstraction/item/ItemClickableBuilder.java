2570258c9bd996dc0835cdbec98782ac98205beb
pixeldev <amiranda8695@gmail.com>
2020-12-03 20:49:31 -0600

    🐛 Null check added to action.

--- abstraction/src/main/java/team/unnamed/gui/abstraction/item/ItemClickableBuilder.java
+++ abstraction/src/main/java/team/unnamed/gui/abstraction/item/ItemClickableBuilder.java
@@ -9,9 +9,11 @@ import static team.unnamed.validate.Validate.notNull;
 
 public class ItemClickableBuilder {
 
+    private static final Predicate<InventoryClickEvent> EMPTY_ACTION = event -> false;
+
     private final int slot;
     private ItemStack itemStack;
-    private Predicate<InventoryClickEvent> action = event -> false;
+    private Predicate<InventoryClickEvent> action = EMPTY_ACTION;
 
     public ItemClickableBuilder(int slot) {
         this.slot = slot;
@@ -24,7 +26,7 @@ public class ItemClickableBuilder {
     }
 
     public ItemClickableBuilder setAction(Predicate<InventoryClickEvent> action) {
-        this.action = action;
+        this.action = action != null ? action : EMPTY_ACTION;
 
         return this;
     }
