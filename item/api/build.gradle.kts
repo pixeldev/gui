9ddd89bbec3e3e33c3d5f11e6459323e354e51d5
pixeldev <pixeldev1729@gmail.com>
2022-07-05 21:13:05 -0500

    deps(item-api): use spigot 1.11.2 as base

--- item/api/build.gradle.kts
+++ item/api/build.gradle.kts
@@ -8,5 +8,5 @@ dependencies {
         api("team.unnamed:commons-$it:3.1.0")
     }
 
-    compileOnly("org.spigotmc:spigot:1.8.8-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot:1.11.2-R0.1-SNAPSHOT")
 }
